05b615f13a7dfbe7856393ec383f3bbbc3d78498
Kota Nonaka <kota.kaicho@gmail.com>
2018-02-25 14:56:15 +0900

    Add is_draft column to article table.

--- db/migrate/20180225054212_create_articles.rb
+++ db/migrate/20180225054212_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.1]
     create_table :articles do |t|
       t.string :title
       t.string :body
+      t.boolean :is_draft
       t.belongs_to :user, foreign_key: true
 
       t.timestamps
