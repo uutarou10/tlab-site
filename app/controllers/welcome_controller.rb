ebb18d20991fbfbc13345135766a07a88afe074f
Kota Nonaka <kota.kaicho@gmail.com>
2018-03-04 12:12:06 +0900

    Add articles view.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    @articles = [
+        {
+            date: '2018/02/02',
+            title: 'hogefugaほげふが'
+        },
+        {
+            date: '2018/02/02',
+            title: 'hogefugaほげふが'
+        }
+    ]
   end
 end
