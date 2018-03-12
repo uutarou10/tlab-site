dc961c5001eec5320a4208b46eafaf5392a2edee
Kota Nonaka <kota.kaicho@gmail.com>
2018-03-12 19:26:00 +0900

    Add article api.

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::ArticlesController < ApplicationController
+  protect_from_forgery
+
+  def index
+    render json: Article.all
+  end
+
+  def create
+    article = Article.new(article_params)
+    article.user = User.first # TODO: ログイン中のユーザーを指定する
+    article.published_at = Time.now
+    article.save!
+
+    render json: article
+  end
+
+  def show
+    article = Article.find(params[:id])
+
+    render json: article
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+    head 204
+  end
+
+  private
+
+  def article_params
+    params.permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   resources :contact, only: [:index]
   resources :articles, only: [:index, :show]
 
+  namespace :api do
+    namespace :v1 do
+      resources :articles, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
