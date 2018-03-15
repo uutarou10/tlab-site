f748304fd986a65ed6c3f7f58d1a67f9a6d2d9b7
Kota Nonaka <kota.kaicho@gmail.com>
2018-03-15 19:04:52 +0900

    Article upload.

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -9,6 +9,13 @@ class Admin::ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new
+    @article.user = User.first # TODO: ログイン中のユーザー
+    @article.title = params[:article][:title]
+    @article.body  = params[:article][:body].read.force_encoding('utf-8')
+    @article.published_at = Time.current
+    @article.save!
+    redirect_to 'index'
   end
 
   def edit
