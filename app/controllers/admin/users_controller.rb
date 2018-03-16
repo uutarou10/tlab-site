eb9e55748759a13fa3ba2f8cbd038a933a5954c2
Kota Nonaka <kota.kaicho@gmail.com>
2018-03-17 02:03:20 +0900

    Add user index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,19 +1,36 @@
 class Admin::UsersController < ApplicationController
+  layout 'admin/layouts/application'
   def index
+    @users = User.all
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save!
   end
 
   def new
+    @user = User.new
   end
 
   def destroy
+    User.destroy(params[:id])
+    redirect_to '/admin/users'
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    @user.save!
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation, :display_name)
   end
 end
